e4c7d29a7198500f4c5e48002a3c0b4060c76c58
dariansaric <darian.saric@fer.hr>
2019-06-03 12:17:46 +0200

    Dodana funkcionalnost ispisa grupa resursa

--- base-recon.ps1
+++ base-recon.ps1
@@ -1,14 +1,40 @@
 ﻿# Import-Module Az
 # autentikacija
 # $args
-Write-Output('[*]Connecting to Azure with custom credentials...')
-$connection = Connect-AzAccount
-if($Null -eq $connection) {
-    Write-Output('[-]Login failed, check your credentials and try again!')
-    Return
+
+function checkSession {
+    $Error.Clear()
+    $context = Get-AzContext -ErrorAction Continue
+    if($Null -eq $context) {
+        Write-Output('[X]You are not logged in on any Azure service, login with a username and password...')
+        Write-Output('[*]Logging in to Azure with custom credentials...')
+        $connection = Connect-AzAccount
+        if($Null -eq $connection) {
+            Write-Output('[-]Login failed, check your credentials and try again!')
+            Return $Null
+        }
+        Write-Output('[+]Logged in as  ' + $acc.Id)
+        $context = Get-AzContext -ErrorAction Continue
+    }
+
+    return $context
 }
+
+function dumpResourceGroups {
+    param( [Microsoft.Azure.Commands.Profile.Models.Core.PSAzureContext]$context)
+    if($Null -eq $context) {
+        Return $Null
+    }
+    Write-Output('[*]Listing all available resource groups...')
+    $groups = Get-AzResourceGroup
+    $groups
+    Return $groups
+}
+
+
+$context = checkSession
 # ispis osnovnih podataka autentikacije
-$acc = $connection.Context.Account
+$acc = $context.Account
 Write-Output('[+]Logged in as  ' + $acc.Id)
 Write-Output('[*]Logged user data: ')
 $acc
@@ -16,12 +42,12 @@ $acc
 Write-Output('[+]Found ' + $acc.ExtendedProperties.Count + ' active/available subscriptions/tenants')
 $acc.ExtendedProperties
 
-
-$context = Get-AzContext
 write-output('[*]Subscription data for active subscription: ' + $context.Subscription.Id)
 $context.Subscription
 $subName = $context.Subscription.Name
 $subId = $context.Subscription.Id
 # todo: ispis resursa za subscription
+# todo: ispis grupa resursa
 
-Disconnect-AzAccount
+dumpResourceGroups -context $context
+# Disconnect-AzAccount
