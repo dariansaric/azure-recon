bb995183f913422459629a9cb1124f77f9d42a58
dariansaric <darian.saric@fer.hr>
2019-06-03 12:36:55 +0200

    Promjene ispisa

--- base-recon.ps1
+++ base-recon.ps1
@@ -4,28 +4,28 @@
 
 function checkSession {
     $Error.Clear()
-    $context = Get-AzContext -ErrorAction Continue
+    $context = Get-AzContext
     if($Null -eq $context) {
-        Write-Output('[X]You are not logged in on any Azure service, login with a username and password...')
-        Write-Output('[*]Logging in to Azure with custom credentials...')
+        # todo: ne radi ispis čuda u funkciji
+        "[X]You are not logged in on any Azure service, login with a username and password..."
+        '[*]Logging in to Azure with custom credentials...'
         $connection = Connect-AzAccount
         if($Null -eq $connection) {
-            Write-Output('[-]Login failed, check your credentials and try again!')
+            '[-]Login failed, check your credentials and try again!'
             Return $Null
         }
-        Write-Output('[+]Logged in as  ' + $acc.Id)
         $context = Get-AzContext -ErrorAction Continue
     }
-
+    '[+]Logged in as  ' + $context.Account.Id
     return $context
 }
 
 function dumpResourceGroups {
-    param( [Microsoft.Azure.Commands.Profile.Models.Core.PSAzureContext]$context)
+    # param( [Microsoft.Azure.Commands.Profile.Models.Core.PSAzureContext]$context)
     if($Null -eq $context) {
         Return $Null
     }
-    Write-Output('[*]Listing all available resource groups...')
+    '[*]Listing all available resource groups...'
     $groups = Get-AzResourceGroup
     $groups
     Return $groups
@@ -33,21 +33,20 @@ function dumpResourceGroups {
 
 
 $context = checkSession
-# ispis osnovnih podataka autentikacije
+
 $acc = $context.Account
-Write-Output('[+]Logged in as  ' + $acc.Id)
-Write-Output('[*]Logged user data: ')
+'[+]Logged in as  ' + $acc.Id
+'[*]Logged user data: '
 $acc
-# todo:
-Write-Output('[+]Found ' + $acc.ExtendedProperties.Count + ' active/available subscriptions/tenants')
+'[+]Found ' + $acc.ExtendedProperties.Count + ' active/available subscriptions/tenants'
 $acc.ExtendedProperties
 
-write-output('[*]Subscription data for active subscription: ' + $context.Subscription.Id)
+'[*]Subscription data for active subscription: ' + $context.Subscription.Id
 $context.Subscription
 $subName = $context.Subscription.Name
 $subId = $context.Subscription.Id
 # todo: ispis resursa za subscription
-# todo: ispis grupa resursa
 
-dumpResourceGroups -context $context
+dumpResourceGroups 
+#-context $context
 # Disconnect-AzAccount
