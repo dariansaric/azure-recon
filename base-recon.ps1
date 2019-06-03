6585a8e212ac499d4608347969b05a4374741ddd
dariansaric <darian.saric@fer.hr>
2019-06-03 11:16:48 +0200

    Popravljena osnovna autentikacija, napravljen ispis osnovnih podataka računa i pretplate

--- base-recon.ps1
+++ base-recon.ps1
@@ -1,6 +1,27 @@
-﻿Import-Module Az
+﻿# Import-Module Az
+# autentikacija
+# $args
+Write-Output('[*]Connecting to Azure with custom credentials...')
+$connection = Connect-AzAccount
+if($Null -eq $connection) {
+    Write-Output('[-]Login failed, check your credentials and try again!')
+    Return
+}
+# ispis osnovnih podataka autentikacije
+$acc = $connection.Context.Account
+Write-Output('[+]Logged in as  ' + $acc.Id)
+Write-Output('[*]Logged user data: ')
+$acc
+# todo:
+Write-Output('[+]Found ' + $acc.ExtendedProperties.Count + ' active/available subscriptions/tenants')
+$acc.ExtendedProperties
 
-Connect-AzAccount
-Write-Output('Hello World')
-Get-AzContext
+
+$context = Get-AzContext
+write-output('[*]Subscription data for active subscription: ' + $context.Subscription.Id)
+$context.Subscription
+$subName = $context.Subscription.Name
+$subId = $context.Subscription.Id
+# todo: ispis resursa za subscription
+
 Disconnect-AzAccount
